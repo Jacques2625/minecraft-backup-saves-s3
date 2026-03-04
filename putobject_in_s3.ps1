82f6ece0705cfb4d9fd55fbf1fcbc6e03bec82b3
jj <124586020+Jacques2625@users.noreply.github.com>
2026-03-04 21:25:25 +0100

    Streamline S3 upload with zip streaming
    Refactor S3 upload process to use zip streaming instead of temporary files.

--- putobject_in_s3.ps1
+++ putobject_in_s3.ps1
@@ -5,12 +5,12 @@
 $InstancePath = "C:\Users\jacqu\AppData\Roaming\PrismLauncher\instances\forever world - jacques\minecraft"
 
 $BucketName = "minecraft-jacques-s3-paris"
-$S3Prefix = "prism_launcher/instances"
-$S3Key = "forever_world_jacques.zip"
-$Profile = "s3_put_minecraft"
+$S3Key = "prism_launcher/instances/forever_world_jacques.zip"
 
-$TempFolder = "$env:TEMP\mc_instance_temp"
-$ZipFile = "$env:TEMP\forever_world_jacques.zip"
+$Profile = "s3_put_minecraft"
+$Region = "eu-west-3"
+
+$SevenZip = "C:\Program Files\7-Zip\7z.exe"
 
 # ==================================
 # WAIT FOR MINECRAFT TO CLOSE
@@ -19,50 +19,27 @@ $ZipFile = "$env:TEMP\forever_world_jacques.zip"
 Write-Host "Surveillance de Minecraft..."
 
 while (Get-Process javaw -ErrorAction SilentlyContinue) {
-    Start-Sleep -Seconds 5
+    Start-Sleep -Seconds 2
 }
 
-Write-Host "Minecraft fermé -> Lancement backup..."
+Write-Host "Minecraft ferme -> Lancement backup..."
 
 # ==================================
-# COPY WITH EXCLUSIONS
+# ZIP STREAMING → S3
 # ==================================
 
-if (Test-Path $TempFolder) {
-    Remove-Item $TempFolder -Recurse -Force
-}
-
-New-Item -ItemType Directory -Path $TempFolder | Out-Null
-
-# Robocopy exclusions
-robocopy $InstancePath $TempFolder `
-    /E `
-    /XD logs downloads `
-    /NFL /NDL /NJH /NJS /nc /ns /np
-
-# ==================================
-# 7-ZIP
-# ==================================
-
-& "C:\Program Files\7-Zip\7z.exe" a -tzip $ZipFile "$TempFolder\*"
-
-# ==================================
-# UPLOAD
-# ==================================
-
-aws s3 cp $ZipFile s3://$BucketName/$S3Prefix/$S3Key --profile $Profile --region eu-west-3
+& "$SevenZip" a -tzip -mx=1 -so "$InstancePath\*" -xr!logs -xr!downloads |
+aws s3 cp - "s3://$BucketName/$S3Key" `
+    --profile $Profile `
+    --region $Region
 
 if ($LASTEXITCODE -eq 0) {
 
     Write-Host "Upload reussi !!!"
-    
-    Remove-Item $TempFolder -Recurse -Force
-    Remove-Item $ZipFile -Force
-
-    Write-Host "Le script a fait son boulot!."
     exit 0
 }
 else {
+
     Write-Host "ERREUR upload!"
     exit 1
 }

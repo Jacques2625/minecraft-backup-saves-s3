8dc9237ff6fd9e45777896b5fd86e0aad119acf4
jj <124586020+Jacques2625@users.noreply.github.com>
2026-03-20 23:20:36 +0100

    Refactor S3 backup script for improved clarity

--- putobject_in_s3.ps1
+++ putobject_in_s3.ps1
@@ -12,34 +12,32 @@ $Region = "eu-west-3"
 
 $SevenZip = "C:\Program Files\7-Zip\7z.exe"
 
-# ==================================
-# WAIT FOR MINECRAFT TO CLOSE
-# ==================================
+# taille estimée du backup (important pour multipart)
+$ExpectedSize = 3000000000
 
-Write-Host "Surveillance de Minecraft..."
-
-while (Get-Process javaw -ErrorAction SilentlyContinue) {
-    Start-Sleep -Seconds 2
-}
-
-Write-Host "Minecraft ferme -> Lancement backup..."
 
 # ==================================
-# ZIP STREAMING → S3
+# STREAM ZIP → S3
 # ==================================
 
-& "$SevenZip" a -tzip -mx=1 -so "$InstancePath\*" -xr!logs -xr!downloads |
-aws s3 cp - "s3://$BucketName/$S3Key" `
-    --profile $Profile `
-    --region $Region
+$cmd = "`"$SevenZip`" a -tzip -mx=1 -mmt=on -so `"$InstancePath\*`" -xr!logs -xr!downloads | aws s3 cp - s3://$BucketName/$S3Key --expected-size $ExpectedSize --profile $Profile --region $Region"
+
+cmd /c $cmd
+
+# ==================================
+# RESULT
+# ==================================
 
 if ($LASTEXITCODE -eq 0) {
 
-    Write-Host "Upload reussi !!!"
+    Write-Host ""
+    Write-Host "Upload reussi vers S3"
+    Write-Host "Backup termine"
     exit 0
 }
 else {
 
-    Write-Host "ERREUR upload!"
+    Write-Host ""
+    Write-Host "ERREUR upload S3"
     exit 1
 }
